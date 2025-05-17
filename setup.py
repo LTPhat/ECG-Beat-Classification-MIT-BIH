c53d1d49b1abecf009f67cd0c13410c8e9b3ae3c
Lam Thanh Phat <lamthanhphat0918398911@gmail.com>
2025-05-18 02:15:12 +0700

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools                                                                                                         
+                                                                                                                          
+setuptools.setup(                                                                                                         
+    name="btl-dtys",                                                                                                  
+    version="0.0.1",                                                                                                      
+    author="Phat Lam",                                                                                                                                                                              
+    description="",    
+    packages=setuptools.find_packages()                                                                                   
+    )                                                                                                                     
